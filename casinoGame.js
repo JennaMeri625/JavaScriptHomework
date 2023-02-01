5f8ced363aa2247ed407ea6fe5002ee5d8710500
Jennifer Inskip <jennifer.m.inskip@gmail.com>
2023-02-01 14:21:44 -0500

    Works up to functions

--- casinoGame.js
+++ casinoGame.js
@@ -1,5 +1,4 @@
 
-
 const prompt = require('prompt-sync')();
 
 const characterName = prompt(`What is your name? `);
@@ -13,27 +12,29 @@ if (wantsToPlay === "n" || wantsToPlay === "N") {
 
 let balance = 10;
 
+let betAmount = 0;
+
 if (balance > 0){
-const betAmount = prompt(`How much would you like to bet? `)
-}else {console.log(`I'm sorry, you do not have sufficient funds to continue.`);
+    betAmount = prompt(`How much would you like to bet? `)
+}else if (balance <= 0){
+    console.log(`I'm sorry, you do not have sufficient funds to continue.`);
         process.exit();
 }
 
-{let betAmount = prompt;
 if (balance<betAmount){
-    console.log(prompt(`I'm sorry, you do not have sufficient funds.  Enter another amount. `));
-}else{
-    let bet = null;
-    do {
+    prompt(`I'm sorry, you do not have sufficient funds.  Enter another amount. `);
+}
+
+let bet = null;
+
+do {
 	bet = prompt("Heads or Tails? [H/T] ");
     }
     while(bet !== "t" && bet !== "T" && bet !== "h" && bet !== "H");
-}
-}
 
-const flip = Math.random() < 0.5;
+    const flip = Math.random() < 0.5;
 
-if (flip === true) {
+    if (flip === true) {
 	console.log('You win!');
 } else {
 	console.log('You lose!');
